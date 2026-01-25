a367bd7b66af85441e2bb26c75f9e0e4bdc14edd
Riccardo Carlesso <ricc+chezmoi@google.com>
2026-01-25 16:23:36 +0100

    upgraded some program of the day

--- rubycon.it/_includes/dsl/content.rb
+++ rubycon.it/_includes/dsl/content.rb
@@ -6,34 +6,41 @@ Rubycon::Schedule.agenda_of_the_day(Date.new(2026, 5, 8)) do
 
   check_in at: "08:30", desc: "Check-in & Welcome Coffee"
   
-  talk at: "09:30", desc: "Opening Remarks", by: "Riccardo Carlesso" # , tags: %w{quick_intro}
+  talk at: "09:30", desc: "Opening Remarks", by: "Riccardo Carlesso" 
   
-  talk at: "09:45", desc: "Opening Keynote", by: "Carmine Paolino" # , tags: %w{keynote}
-  talk at: "10:30", desc: "Speech #2",    by: "Julia Lopez"
+  # Note, we might have a different keynote speaker
+  talk at: "09:45", by: "Carmine Paolino", 
+       title: "Ruby Is the Best Language for Building AI Web Apps"
+  talk at: "10:30", by: "Julia Lopez",  title: "Debugging Techniques in Ruby:" +
+           "You still need to understand how the code works!"
   
   pause at: "11:15", desc: "☕ Coffee Break"
   
-  talk at: "11:30", desc: "Speech #3",   by: "Marco Roth"
-  talk at: "12:15", desc: "Speech #4" #,    by: "TBD"
+  talk at: "11:30", by: "Marco Roth",
+       title: something_on(:Herb, :ReActionView)
+  talk at: "12:15", title: "Speech #4" # TBD
   
   pause at: "13:00", desc: "🍝 Lunch Break & Networking"
   
-  # We wamt to give everyone the chance to say/bring something for 5 minutes.
+  # We want to give everyone the chance to say/bring something for 5 minutes.
   lightning_talks at: "14:30", desc: "5m for everyone who has something to say" , 
                   tags: %w{ FIFO }
-  talk at: "15:15", desc: "Speech #5" #,    by: "TBD"
+  talk at: "15:15", title: "Speech #5"
     
   pause at: "16:00", desc: "☕ Coffee Break"
   
-  talk at: "16:15", desc: "Speech #6"  #,    by: "TBD"
-  talk at: "17:00", desc: "Speech #7" #,    by: "TBD"  
-  talk at: "17:45", desc: "It's a wrap!",    by: "Riccardo, Emiliano, and the team"
+  talk at: "16:15", title: "Speech #6" 
+  talk at: "17:00", title: "Speech #7"  
+  talk at: "17:45", title: "It's a wrap!",    by: "Riccardo, Emiliano, and the team"
 
   # Here everyone will be free to do what they want. Keep the conversation going, 
   # or refill the battiries in the hotel, whatever you prefer. 
   pause at: "18:00", desc: "Drinks or Shower or Nap", tags: %w{optional}
 
-  # Here we will have dinner, and then the Toga Party! The place is 10m walk from the hotel.
+  # Here we will have dinner, and then the Toga Party! 
+  # The place is 10m walk from the hotel.
   party at: "20:00", desc: "🍝 Dinner + 🎉 Toga Party!",    by: "All", 
-        where: '🏖️ Bagno 46'
+        where: '🏖️ Bagno 46', url: 'https://www.bagno46rimini.com/'
 end
+
+# Titles being refined here: https://github.com/palladius/rubycon.it/issues/74
