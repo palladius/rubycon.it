2599ca7226a76fe6d236eb47ffb08a5c69a6d559
a-chris <a.christian.toscano@gmail.com>
2026-02-25 00:08:49 +0100

    improve agenda layout

--- rubycon.it/_includes/dsl/content.rb
+++ rubycon.it/_includes/dsl/content.rb
@@ -2,45 +2,69 @@
 
 # Riccardo loves Ruby DSL even more than my very first aDSL!
 
-Rubycon::Schedule.agenda_of_the_day(Date.new(2026, 5, 8)) do
+Rubycon::Agenda.schedule(Date.new(2026, 5, 8)) do
 
-  check_in at: "08:30", desc: "Check-in & Welcome Coffee"
-  
-  talk at: "09:30", desc: "Opening Remarks", by: "Riccardo Carlesso" 
-  
-  # Note, we might have a different keynote speaker
-  talk at: "09:45", by: "Carmine Paolino", 
-       title: "Ruby Is the Best Language for Building AI Web Apps"
-  talk at: "10:30", by: "Julia Lopez",  title: "Debugging Techniques in Ruby:" +
-           "You still need to understand how the code works!"
-  
-  pause at: "11:15", desc: "☕ Coffee Break"
-  
-  talk at: "11:30", by: "Marco Roth",
-       title: something_on(:Herb, :ReActionView)
-  talk at: "12:15", title: "Speech #4" # TBD
-  
-  pause at: "13:00", desc: "🍝 Lunch Break & Networking"
-  
+ check_in at: "08:30",
+     desc: "Check-in & Welcome Coffee"
+
+ talk at: "09:30",
+     desc: "Opening Remarks",
+     by: "Riccardo Carlesso"
+
+  talk at: "09:45",
+     by: "Carmine Paolino",
+     title: "Ruby Is the Best Language for Building AI Web Apps"
+
+  talk at: "10:30",
+     by: "Julia Lopez",
+     title: "Debugging Techniques in Ruby: You still need to understand how the code works!"
+
+  pause at: "11:15",
+     desc: "☕️ Coffee Break"
+
+  talk at: "11:30",
+     by: "Marco Roth",
+     title: something_on(:Herb, :ReActionView)
+
+  talk at: "12:15",
+     title: "Speech #4"
+
+  pause at: "13:00",
+     desc: "🍝 Lunch Break & Networking"
+
   # We want to give everyone the chance to say/bring something for 5 minutes.
-  lightning_talks at: "14:30", desc: "5m for everyone who has something to say" , 
-                  tags: %w{ FIFO }
-  talk at: "15:15", title: "Speech #5"
-    
-  pause at: "16:00", desc: "☕ Coffee Break"
-  
-  talk at: "16:15", title: "Speech #6" 
-  talk at: "17:00", title: "Speech #7"  
-  talk at: "17:45", title: "It's a wrap!",    by: "Riccardo, Emiliano, and the team"
+  lightning_talks at: "14:30",
+     desc: "⚡️ 5m for everyone who has something to say" ,
+     tags: %w{ FIFO }
 
-  # Here everyone will be free to do what they want. Keep the conversation going, 
-  # or refill the battiries in the hotel, whatever you prefer. 
-  pause at: "18:00", desc: "Drinks or Shower or Nap", tags: %w{optional}
+  talk at: "15:15",
+     title: "Speech #5"
 
-  # Here we will have dinner, and then the Toga Party! 
+  pause at: "16:00",
+     desc: "☕ Coffee Break"
+
+  talk at: "16:15",
+     title: "Speech #6"
+
+  talk at: "17:00",
+     title: "Speech #7"
+
+  talk at: "17:45",
+     title: "It's a wrap!",
+     by: "Riccardo, Emiliano, and the team"
+
+  # Keep the conversation going, or refill
+  # the batteries in the hotel, whatever you prefer.
+  pause at: "18:00",
+     desc: "Drinks or Shower or Nap",
+     tags: %w{optional}
+
+  # Here we will have dinner, and then the Toga Party!
   # The place is 10m walk from the hotel.
-  party at: "20:00", desc: "🍝 Dinner + 🎉 Toga Party!",    by: "All", 
-        where: '🏖️ Bagno 46', url: 'https://www.bagno46rimini.com/'
+  party at: "20:00",
+     desc: "🍝 Dinner + 🎉 Toga Party!",
+     where: '🏖️ Bagno 46',
+     url: 'https://www.bagno46rimini.com/'
 end
 
 # Titles being refined here: https://github.com/palladius/rubycon.it/issues/74
